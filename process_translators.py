ff57f7716f88e85aa890a7678338bfbab63e5e6e
Zeeecks <zihengxu139@gmail.com>
2022-09-14 22:57:00 -0400

    updated metadata extractor

--- process_translators.py
+++ process_translators.py
@@ -40,26 +40,20 @@ for filename in glob.glob("translators/*.js"):
     script = contents.split("***** END LICENSE BLOCK *****\n*/")[1].strip().split("/** BEGIN TEST CASES **/")[0].strip()
     with open(f"repo/{translatorID}", "w") as outfile:
       outfile.write(script)
-    metadata.append(str(json.loads(meta_string)))  
+    metadata.append(json.loads(meta_string))  
   except:
     try:
       script = contents.split("\"\n}\n")[1].strip().split("/** BEGIN TEST CASES **/")[0].strip()
       with open(f"repo/{translatorID}", "w") as outfile:
         outfile.write(script)
-      metadata.append(str(json.loads(meta_string)))
+      metadata.append(json.loads(meta_string))
     except:
       print("No script detected in \"" + filename + "\"")
       denylist.append(filename.split("/")[1])
 denylist.sort()
 
 with open(f"repo/metadata", "w") as outfile:
-  outfile.write("[")
-  for i in range(len(metadata)):
-    metadata_entry = metadata[i]
-    outfile.write(metadata_entry)
-    if i < len(metadata) - 1:
-      outfile.write(",\n")
-  outfile.write("]")
+  json.dump(metadata, outfile)
 
 print(str(denylist))
 print(len(denylist), len(metadata), len(glob.glob("repo")) - 1, len(glob.glob("translators/*.js")))
